a4aab5789d01795fa0ccec62110fdd362980138a
Thomas Debray <thomasdebray83@gmail.com>
2023-06-20 12:56:09 +0200

    Adding product model and controller

---
+++ SelXPressApi/SelXPressApi/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelXPressApi.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        /// <summary>
+        /// Add a product to the database
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult AddProduct()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Modify the product in the database
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult ModifyProduct()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Delete the product from the database
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult DeleteProduct()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get all products from database
+        /// </summary>
+        /// <returns> all products</returns>
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Get one product from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SelXPressApi/SelXPressApi/Models/ProductModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SelXPressApi.Models
+{
+    public class ProductModel
+    {
+        /// <summary>
+        /// Product Model
+        /// Id is the id of the product PK not null
+        /// ProductName is the name of the product varchar(200) not null
+        /// ProductDescription is the description of the product varchar(1000) not null
+        /// ProductPrice is the price of the product float not null
+        /// ProductCategoryId is the id of the category FK references Categories(id) not null
+        /// ProductPicture is the picture of the product varchar(500) not null
+        /// CreatedAt is the date of creation of the product datetime not null
+        /// sellPeopleId is the id of the seller FK references SellPeople(id) not null
+        /// </summary>
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public float ProductPrice { get; set; }
+        public string ProductCategoryId { get; set; }
+        public string ProductPicture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int sellPeopleId { get; set; }
+    }
+}
