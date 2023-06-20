ad2d47245132379964587626a61d50a0afc462f5
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-06-20 17:48:04 +0200

    modification de la base de données et ajout d'un règle pour le gitignore

--- SelXPressApi/database-files/database-template.sql
+++ SelXPressApi/database-files/database-template.sql
@@ -1,32 +1,32 @@
-drop table if exists Roles, Users, Categories, Tags, Products, Stocks, Orders, OrderProducts, Carts cascade ;
+drop table if exists Roles, Users, Categories, Tags, Products, Stocks, Orders, OrderProducts, Carts, Comments, Mark cascade ;
 
 CREATE TABLE Roles (
-                       id int primary key not null,
+                       id int primary key not null auto_increment,
                        name varchar(200) not null
 );
 CREATE TABLE Users (
-                       id INT primary key not null,
-                       username varchar(150) not null ,
+                       id INT primary key not null auto_increment,
+                       username varchar(150) not null unique ,
                        firstname varchar(100),
                        lastname varchar(100),
                        password varchar(200) not null ,
-                       email varchar(200) not null ,
+                       email varchar(200) not null unique ,
                        roleId int references Roles(id)
 );
 
 CREATE TABLE Categories (
-                            id int primary key not null,
-                            name varchar(200) not null
+                            id int primary key not null auto_increment,
+                            name varchar(200) not null unique
 );
 
 CREATE TABLE Tags (
-                      id int primary key not null,
-                      name varchar(200) not null ,
+                      id int primary key not null auto_increment,
+                      name varchar(200) not null unique ,
                       categoryId int references Categories(id)
 );
 
 CREATE TABLE Products (
-                          id int primary key not null,
+                          id int primary key not null auto_increment,
                           name varchar(200) not null ,
                           description varchar(1000) not null,
                           price float,
@@ -37,25 +37,38 @@ CREATE TABLE Products (
 );
 
 CREATE TABLE Stocks (
-                        id int primary key not null ,
+                        id int primary key not null auto_increment,
                         productId int references Products(id),
                         quantity int not null
 );
 
 CREATE TABLE Orders (
-                        id int primary key not null ,
+                        id int primary key not null auto_increment ,
                         userId int references Users(id),
                         totalPrice float not null
 );
 
 CREATE TABLE OrderProducts (
-                               id int primary key not null ,
+                               id int primary key not null auto_increment,
                                productId int references Products(id),
                                orderId int references Orders(id)
 );
 
 CREATE TABLE Carts (
-                       id int primary key not null ,
+                       id int primary key not null auto_increment,
                        userId int references Users(id),
                        productId int references Products(id)
+);
+
+CREATE TABLE Comments (
+                          id int primary key not null auto_increment,
+                          message varchar(500) not null ,
+                          userId int references Users(id),
+                          productId int references Products(id)
+);
+
+CREATE TABLE Mark (
+                      id int primary key not null auto_increment,
+                      rate float not null ,
+                      commentId int references Comments(id)
 );
