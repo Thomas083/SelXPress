b81e58ebc0108484d74b31f92df24c37c6759b7f
Julien Lamalle <julien1.lamalle@epitech.eu>
2023-06-27 16:18:24 +0200

    init vuejs3 project

---
+++ sell_x_press_front_office/src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="maxence">
+      <p class="maxence-p">
+        COUCOU MAXENCE ! 
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .about {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .maxence {
+    background-color: pink;
+    border-radius: 20px;
+    height: 30%;
+    width: 30%;
+  }
+  .maxence-p {
+    font-size: 30px;
+  }
+</style>
