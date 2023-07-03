3412817dd1e1c86a9d87c9e73ff032b4f8f5d6a4
Thomas Debray <thomas.debray@epitech.eu>
2023-07-03 17:23:26 +0200

    Adding summary to Attribute controller

--- SelXPressApi/SelXPressApi/Controllers/AttributeController.cs
+++ SelXPressApi/SelXPressApi/Controllers/AttributeController.cs
@@ -8,33 +8,53 @@ namespace SelXPressApi.Controllers
 	[ApiController]
 	public class AttributeController : ControllerBase
 	{
-		// GET: api/<AttributeController>
+		/// <summary>
+		/// GET: api/<AttributeController>
+		/// </summary>
+		/// <returns>Array of all attributes</returns>
 		[HttpGet]
 		public IEnumerable<string> Get()
 		{
 			return new string[] { "value1", "value2" };
 		}
 
-		// GET api/<AttributeController>/5
+		/// <summary>
+		/// GET api/<AttributeController>/5
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>The attribute</returns>
 		[HttpGet("{id}")]
 		public string Get(int id)
 		{
 			return "value";
 		}
 
-		// POST api/<AttributeController>
+		/// <summary>
+		/// POST api/<AttributeController>
+		/// create new attribute
+		/// </summary>
+		/// <param name="value"></param>
 		[HttpPost]
 		public void Post([FromBody] string value)
 		{
 		}
 
-		// PUT api/<AttributeController>/5
+		/// <summary>
+		/// PUT api/<AttributeController>/5
+		/// modify an attribute
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="value"></param>
 		[HttpPut("{id}")]
 		public void Put(int id, [FromBody] string value)
 		{
 		}
 
-		// DELETE api/<AttributeController>/5
+		/// <summary>
+		/// DELETE api/<AttributeController>/5
+		/// delete an attribute
+		/// </summary>
+		/// <param name="id"></param>
 		[HttpDelete("{id}")]
 		public void Delete(int id)
 		{
