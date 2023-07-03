0609e681a04bf9a2c05ae09e2c15764a66338e1d
Debray Thomas <thomasdebray83@gmail.com>
2023-07-03 17:48:09 +0200

    Adding summary to OrderController

--- SelXPressApi/SelXPressApi/Controllers/OrderController.cs
+++ SelXPressApi/SelXPressApi/Controllers/OrderController.cs
@@ -8,33 +8,55 @@ namespace SelXPressApi.Controllers
 	[ApiController]
 	public class OrderController : ControllerBase
 	{
-		// GET: api/<OrderController>
+		/// <summary>
+		/// GET: api/<OrderController>
+		/// Get all orders
+		/// </summary>
+		/// <returns>Return an Array of all orders</returns>
 		[HttpGet]
 		public IEnumerable<string> Get()
 		{
 			return new string[] { "value1", "value2" };
 		}
 
-		// GET api/<OrderController>/5
+		/// <summary>
+		/// GET api/<OrderController>/5
+		/// Get a specific order
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>Return a specific order</returns>
 		[HttpGet("{id}")]
 		public string Get(int id)
 		{
 			return "value";
 		}
 
-		// POST api/<OrderController>
+		/// <summary>
+		/// POST api/<OrderController>
+		/// Create a new order
+		/// </summary>
+		/// <param name="value"></param>
 		[HttpPost]
 		public void Post([FromBody] string value)
 		{
 		}
 
-		// PUT api/<OrderController>/5
+		/// <summary>
+		/// PUT api/<OrderController>/5
+		/// Modify a specific order
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="value"></param>
 		[HttpPut("{id}")]
 		public void Put(int id, [FromBody] string value)
 		{
 		}
 
-		// DELETE api/<OrderController>/5
+		/// <summary>
+		/// DELETE api/<OrderController>/5
+		/// Delete a specific order
+		/// </summary>
+		/// <param name="id"></param>
 		[HttpDelete("{id}")]
 		public void Delete(int id)
 		{
