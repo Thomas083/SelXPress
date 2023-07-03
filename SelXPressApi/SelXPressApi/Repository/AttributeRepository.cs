ce0cdc2743d87f29ad104a0346d3d4b18b510d13
ugo-bertrand <113342194+ugo-bertrand@users.noreply.github.com>
2023-07-03 14:44:36 +0200

    create the repository for all models and inject the data

---
+++ SelXPressApi/SelXPressApi/Repository/AttributeRepository.cs
@@ -0,0 +1,21 @@
+﻿using SelXPressApi.Data;
+using SelXPressApi.Interfaces;
+using Attribute = SelXPressApi.Models.Attribute;
+
+namespace SelXPressApi.Repository
+{
+    public class AttributeRepository : IAttributeRepository
+    {
+        private readonly DataContext _context;
+
+        public AttributeRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Attribute> GetAllAttributes()
+        {
+            return _context.Attributes.OrderBy(a => a.Id).ToList();
+        }
+    }
+}
