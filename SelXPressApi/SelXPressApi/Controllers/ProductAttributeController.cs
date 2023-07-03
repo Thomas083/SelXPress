5937f643fda0fc72f64c9ebc41a3785e29d40520
Debray Thomas <thomasdebray83@gmail.com>
2023-07-03 17:42:18 +0200

    Adding Summary to ProductAttributeController

--- SelXPressApi/SelXPressApi/Controllers/ProductAttributeController.cs
+++ SelXPressApi/SelXPressApi/Controllers/ProductAttributeController.cs
@@ -8,33 +8,55 @@ namespace SelXPressApi.Controllers
 	[ApiController]
 	public class ProductAttributeController : ControllerBase
 	{
-		// GET: api/<ProductAttributeController>
+		/// <summary>
+		/// GET: api/<ProductAttributeController>
+		/// Get all product attributes
+		/// </summary>
+		/// <returns>Return an Array of all product's attribute</returns>
 		[HttpGet]
 		public IEnumerable<string> Get()
 		{
 			return new string[] { "value1", "value2" };
 		}
 
-		// GET api/<ProductAttributeController>/5
+		/// <summary>
+		/// GET api/<ProductAttributeController>/5
+		/// Get a product attribute by id
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns>Return a specific attribute of the product</returns>
 		[HttpGet("{id}")]
 		public string Get(int id)
 		{
 			return "value";
 		}
 
-		// POST api/<ProductAttributeController>
+		/// <summary>
+		/// POST api/<ProductAttributeController>
+		/// Create a new product attribute
+		/// </summary>
+		/// <param name="value"></param>
 		[HttpPost]
 		public void Post([FromBody] string value)
 		{
 		}
 
-		// PUT api/<ProductAttributeController>/5
+		/// <summary>
+		/// PUT api/<ProductAttributeController>/5
+		/// Modify a product attribute
+		/// </summary>
+		/// <param name="id"></param>
+		/// <param name="value"></param>
 		[HttpPut("{id}")]
 		public void Put(int id, [FromBody] string value)
 		{
 		}
 
-		// DELETE api/<ProductAttributeController>/5
+		/// <summary>
+		/// DELETE api/<ProductAttributeController>/5
+		/// Delete a product attribute
+		/// </summary>
+		/// <param name="id"></param>
 		[HttpDelete("{id}")]
 		public void Delete(int id)
 		{
