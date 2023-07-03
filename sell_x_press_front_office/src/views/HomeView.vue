f2b2ce0b2a8a42dc8cf68d06c03cda5b68067706
Maxence Leroy <maxence.leroy@epitech.eu>
2023-07-03 19:45:50 +0200

    Layout : Help Component but not responsive

--- sell_x_press_front_office/src/views/HomeView.vue
+++ sell_x_press_front_office/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <Header />
-  <!-- <Footer/> -->
+  <Footer/>
+  <Help />
 </template>
 
 <script>
 // @ is an alias to /src
 import Header from '@/components/Layout/Header.vue';
 import Footer from '@/components/Layout/Footer.vue';
+import Help from '@/components/Layout/Help.vue'
 
 export default {
   name: 'HomeView',
   components: {
     Header,
-    Footer
+    Footer,
+    Help
   }
 }
 </script>
