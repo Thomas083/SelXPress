55dbe3f44a2ad548595da7b8824e7c3b3ad536b3
Julien Lamalle <julien1.lamalle@epitech.eu>
2023-07-03 12:34:23 +0200

    add bootstrap + axios

--- sell_x_press_front_office/src/main.js
+++ sell_x_press_front_office/src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 import "./main.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const pinia = createPinia();
-createApp(App).use(pinia).use(router).mount('#app');
+const app = createApp(App);
+
+app
+    .use(pinia)
+    .use(router)
+    .use(bootstrap)
+    .mount('#app');
