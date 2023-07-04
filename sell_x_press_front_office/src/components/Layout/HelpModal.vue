7b87726526f3117699e874ae8df3545fae812459
Maxence Leroy <113341289+MaxenceLer59@users.noreply.github.com>
2023-07-04 14:49:03 +0200

    Feature/front office layout (#10)
    * Header and Footer component created but not responsive
    
    * Footer : responsive (1 column if phone)
    
    * Layout : Help Component but not responsive
    
    * Layout : Header Identification Page Created and Rename of the Main Header
    
    * Header : responsive (not at all) and 2 Header if the user is register or not
    
    * Set export default Help component
    
    * change component name for pull request

---
+++ sell_x_press_front_office/src/components/Layout/HelpModal.vue
@@ -0,0 +1,93 @@
+<template>
+    <div class="modal-layout">
+        <div class="modal-container">
+            <div class="modal-title">Help Us To Upgrade</div>
+            <form class="modal-form">
+                <textarea class="modal-form-input" type="text" placeholder="Write your comment here..." ></textarea>
+                <div class="modal-form-btn-container">
+                    <button class="modal-form-btn">
+                        SEND
+                        <img width="20vw" src='../../assets/Modal/send.png' />
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "HelpModal",
+};
+
+</script>
+
+<style scoped>
+.modal-layout {
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: fixed;
+    background: rgba(94, 94, 94, 0.74);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+}
+
+.modal-container {
+    padding: 3rem 5rem;
+    border-radius: 50px;
+    background-color: var(--main-white);
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.modal-title {
+    font-size: 3rem;
+    font-family: url(../../font/Inter/static/Inter-SemiBold.ttf);
+    margin-bottom: 5vh;
+}
+
+.modal-form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    border-radius: 2rem;
+}
+
+.modal-form-input {
+    border-radius: 1rem;
+    padding: 1rem;
+    height: 30vh;
+    resize: none;
+}
+
+.modal-form-btn-container {
+    display: flex;
+    justify-content: flex-end;
+}
+
+.modal-form-btn {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--main-orange);
+    color: var(--main-white);
+    font-family: url(../../font/Inter/static/Inter-SemiBold.ttf);
+    font-size: 1rem;
+    padding: 1rem 3rem;
+    gap: 1rem;
+    border-radius: 1rem;
+    border: none
+}
+
+.modal-form-btn:hover {
+    opacity: 0.5;
+}
+</style>
--- sell_x_press_front_office/src/views/HomeView.vue
+++ sell_x_press_front_office/src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Front Office SelXpress"/>
-  </div>
+  <header-not-registered />
+  <header-regitered />
+  <header-identification-page />
+  <footer-layout />
+  <!-- <help-modal /> -->
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderNotRegistered from '@/components/Layout/HeaderNotRegistered.vue';
+import HeaderRegitered from '@/components/Layout/HeaderRegitered.vue';
+import HeaderIdentificationPage from '@/components/Layout/HeaderIdentificationPage.vue';
+import FooterLayout from '@/components/Layout/FooterLayout.vue';
+import HelpModal from '@/components/Layout/HelpModal.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderNotRegistered,
+    HeaderRegitered,
+    HeaderIdentificationPage,
+    FooterLayout,
+    HelpModal,
   }
 }
 </script>
