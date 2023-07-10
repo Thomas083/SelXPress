149e9af62cb5931d5242c19ce3845ed7dcd2ab32
Maxence Leroy <maxence.leroy@epitech.eu>
2023-07-10 11:21:59 +0200

    Add HeaderNotAdmin header layout

--- sell_x_press_back_office/src/views/HomeView.vue
+++ sell_x_press_back_office/src/views/HomeView.vue
@@ -1,15 +1,19 @@
 <template>
   <HeaderIdentificationPage />
+  <br>
+  <HeaderNotAdmin />
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderIdentificationPage from '@/components/Layout/HeaderIdentificationPage.vue';
+import HeaderNotAdmin from '@/components/Layout/HeaderNotAdmin.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HeaderIdentificationPage
+    HeaderIdentificationPage,
+    HeaderNotAdmin
   }
 }
 </script>
