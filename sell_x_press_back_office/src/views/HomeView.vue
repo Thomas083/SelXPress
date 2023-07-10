27bef6023119b734ae9ca1fd3d54741d6d9cadab
Maxence Leroy <maxence.leroy@epitech.eu>
2023-07-10 11:00:23 +0200

    Add Header for the Identification Page

--- sell_x_press_back_office/src/views/HomeView.vue
+++ sell_x_press_back_office/src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Back Office SelXpress"/>
-  </div>
+  <HeaderIdentificationPage />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderIdentificationPage from '@/components/Layout/HeaderIdentificationPage.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderIdentificationPage
   }
 }
 </script>
