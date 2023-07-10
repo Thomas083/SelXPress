103fb30c20527471ad24c73b4bc5223db757e588
Maxence Leroy <113341289+MaxenceLer59@users.noreply.github.com>
2023-07-10 14:36:30 +0200

    Feature/back office layout (#13)
    * Add Header for the Identification Page
    
    * Add HeaderNotAdmin header layout
    
    * Add HeaderAdmin header when an admin is login
    
    * Add Footer to the HomeView

--- sell_x_press_back_office/src/main.js
+++ sell_x_press_back_office/src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-// import "./main.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./main.css";
 
 const pinia = createPinia();
 const app = createApp(App);
--- sell_x_press_back_office/src/views/HomeView.vue
+++ sell_x_press_back_office/src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Back Office SelXpress"/>
-  </div>
+  <HeaderIdentificationPage />
+  <br>
+  <HeaderNotAdmin />
+  <br>
+  <HeaderAdmin />
+  <br>
+  <FooterLayout />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderIdentificationPage from '@/components/Layout/HeaderIdentificationPage.vue';
+import HeaderNotAdmin from '@/components/Layout/HeaderNotAdmin.vue';
+import HeaderAdmin from '@/components/Layout/HeaderAdmin.vue';
+import FooterLayout from '@/components/Layout/FooterLayout.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HeaderIdentificationPage,
+    HeaderNotAdmin,
+    HeaderAdmin,
+    FooterLayout
   }
 }
 </script>
