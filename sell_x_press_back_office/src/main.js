82d71d029aef2eeaa8f4caac2a129bc8a05efaf2
Lamalle <78221800+JuLamalle@users.noreply.github.com>
2023-07-17 14:07:45 +0200

    add pages + toaster + firebase (#20)

--- sell_x_press_back_office/src/main.js
+++ sell_x_press_back_office/src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import { createPinia } from 'pinia';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./main.css";
+import 'mosha-vue-toastify/dist/style.css'
 
 const pinia = createPinia();
 const app = createApp(App);
