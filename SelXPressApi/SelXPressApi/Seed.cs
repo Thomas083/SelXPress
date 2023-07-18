d37616f880d6a6bf90f673c0b353dff032382916
ugo-bertrand <113342194+ugo-bertrand@users.noreply.github.com>
2023-07-18 14:23:51 +0200

    create the class for the seeding of the database

---
+++ SelXPressApi/SelXPressApi/Seed.cs
@@ -0,0 +1,20 @@
+﻿using SelXPressApi.Data;
+using SelXPressApi.Models;
+
+namespace SelXPressApi;
+
+public class Seed
+{
+    private readonly DataContext dataContext;
+
+    public Seed(DataContext context)
+    {
+        dataContext = context;
+    }
+
+    public void GenerateData()
+    {
+        
+    }
+    
+}
