343c5b79a67678b8baa6ce9e93893e9f23b1971d
Maxence Leroy <maxence.leroy@epitech.eu>
2023-07-18 09:16:10 +0200

    add home page to the back-office

---
+++ sell_x_press_back_office/src/components/categories/CategoriesList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="categories-container">
+            <div class="category">Category#1</div>
+            <div class="separation"></div>
+            <div class="category">Category#2</div>
+            <div class="separation"></div>
+            
+        </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CategoriesList'
+}
+
+</script>
+
+<style scoped>
+
+.categories-container {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--main-white);
+    gap: 0.5rem;
+    border-radius: 15px;
+    padding: 1rem;
+    height: max-content;
+    max-width: 300px;
+    align-items: flex-start;
+}
+
+.category {
+    font-size: 2rem;
+    cursor: pointer;
+}
+
+.category:hover {
+    opacity: 0.7;
+}
+
+.separation {
+    border: 1px var(--main-grey-product) solid;
+    width: 90%;
+}
+
+</style>
--- sell_x_press_back_office/src/views/HomeView.vue
+++ sell_x_press_back_office/src/views/HomeView.vue
@@ -1,12 +1,56 @@
 <template>
+  <div class="home-container">
+    <categories-list />
+    <div class="products-cards-container">
+      <h1 class="products-category">Category: All</h1>
+      <div class="products-cards">
+        <product-card />
+        <product-card />
+        <product-card />
+        <product-card />
+        <product-card />
+        <product-card />
+        <product-card />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
+import CategoriesList from "@/components/categories/CategoriesList.vue"
+import ProductCard from "@/components/products/ProductsCard.vue"
 
 export default {
   name: 'HomeView',
   components: {
+    ProductCard,
+    CategoriesList
   }
 }
 </script>
+
+<style>
+.home-container {
+  padding: 2rem 1rem;
+  display: flex;
+  gap: 3rem;
+}
+
+.products-cards-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1rem;
+}
+
+.products-category {
+  font-weight: bold;
+}
+
+.products-cards {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+}
+</style>
