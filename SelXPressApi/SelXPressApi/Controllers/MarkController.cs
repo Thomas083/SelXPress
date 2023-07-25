dd1af39d560dac711e9cae959aee00c5ee2a4c1d
ugo-bertrand <113342194+ugo-bertrand@users.noreply.github.com>
2023-07-25 11:17:02 +0200

    change the name of the error code in the mark controller

--- SelXPressApi/SelXPressApi/Controllers/MarkController.cs
+++ SelXPressApi/SelXPressApi/Controllers/MarkController.cs
@@ -31,10 +31,10 @@ namespace SelXPressApi.Controllers
 		public async Task<IActionResult> GetAllMarks()
 		{
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1000");
+				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1101");
 			var marks = await _markRepository.GetAllMark();
 			if (marks.Count == 0)
-				throw new NotFoundException("There is no marks in the database, please try again", "MRK-1001");
+				throw new NotFoundException("There is no marks in the database, please try again", "MRK-1401");
 			return Ok(marks);
 		}
 
@@ -54,7 +54,7 @@ namespace SelXPressApi.Controllers
 			if (!await _markRepository.MarkExists(id))
 				throw new NotFoundException("The mark with the id : " + id + " doesn't exist", "MRK-1002");
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1003");
+				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1402");
 			var mark = await _markRepository.GetMarkById(id);
 			return Ok(mark);
 		}
@@ -75,9 +75,9 @@ namespace SelXPressApi.Controllers
 		{
 			var marks = await _markRepository.GetMarkByUser(id);
 			if (marks.Count == 0)
-				throw new NotFoundException("There is no marks for the user with the id : " + id, "MRK-1004");
+				throw new NotFoundException("There is no marks for the user with the id : " + id, "MRK-1403");
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1005");
+				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1101");
 			return Ok(marks);
 		}
 		
@@ -97,9 +97,9 @@ namespace SelXPressApi.Controllers
 		{
 			var marks = await _markRepository.GetMarkByProduct(id);
 			if (marks.Count == 0)
-				throw new NotFoundException("There is no marks for the product with the id : " + id, "1006");
+				throw new NotFoundException("There is no marks for the product with the id : " + id, "MRK-1404");
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1007");
+				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1101");
 			return Ok(marks);
 		}
 
@@ -116,9 +116,9 @@ namespace SelXPressApi.Controllers
 		public async Task<IActionResult> CreateMark([FromBody] CreateMarkDTO markDto)
 		{
 			if (markDto == null)
-				throw new BadRequestException("There is missing fields, please try again with some data", "MRK-1008");
+				throw new BadRequestException("There is missing fields, please try again with some data", "MRK-1102");
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1009");
+				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1101");
 			await _markRepository.CreateMark(markDto);
 			return Ok();
 		}
@@ -139,11 +139,11 @@ namespace SelXPressApi.Controllers
 		public async Task<IActionResult> UpdateMark(int id, [FromBody] UpdateMarkDTO updateMarkDto)
 		{
 			if (updateMarkDto == null)
-				throw new BadRequestException("There is missing fields, please try again with some data", "MRK-1010");
+				throw new BadRequestException("There is missing fields, please try again with some data", "MRK-1102");
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1011");
+				throw new BadRequestException("The model is wrong, a bad request occured", "MRK-1101");
 			if (!await _markRepository.MarkExists(id))
-				throw new NotFoundException("The mark with the id : " + id + " doesn't exist", "MRK-1012");
+				throw new NotFoundException("The mark with the id : " + id + " doesn't exist", "MRK-1402");
 			await _markRepository.UpdateMarkById(updateMarkDto, id);
 			return Ok();
 		}
@@ -161,9 +161,9 @@ namespace SelXPressApi.Controllers
 		public async Task<IActionResult> DeleteMark(int id)
 		{
 			if (!await _markRepository.MarkExists(id))
-				throw new NotFoundException("The mark with the id : " + id + " doesn't exist", "MRK-1013");
+				throw new NotFoundException("The mark with the id : " + id + " doesn't exist", "MRK-1402");
 			if (!ModelState.IsValid)
-				throw new BadRequestException("The model is wrong, a bad request occured","MRK-1014");
+				throw new BadRequestException("The model is wrong, a bad request occured","MRK-1101");
 			await _markRepository.DeleteMarkById(id);
 			return Ok();
 		}
