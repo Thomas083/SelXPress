ef4b8441a917f0a6351ecc97bc124071da834515
Debray Thomas <thomasdebray83@gmail.com>
2023-08-01 10:10:41 +0200

    Adding attribute and attributeData to mappingProfiles

--- SelXPressApi/SelXPressApi/Helper/MappingProfiles.cs
+++ SelXPressApi/SelXPressApi/Helper/MappingProfiles.cs
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 ﻿using AutoMapper;
 using SelXPressApi.DTO.AttributeDataDTO;
 using SelXPressApi.DTO.AttributeDTO;
-=======
-using AutoMapper;
-using SelXPressApi.DTO.CategoryDTO;
-using SelXPressApi.DTO.CommentDTO;
->>>>>>> develop
 using SelXPressApi.DTO.UserDTO;
 using SelXPressApi.Models;
 using Attribute = SelXPressApi.Models.Attribute;
@@ -19,13 +13,8 @@ namespace SelXPressApi.Helper
         {
             CreateMap<User, UserDto>();
             CreateMap<User, CreateUserDto>();
-<<<<<<< HEAD
             CreateMap<Attribute, AttributeDTO>();
             CreateMap<AttributeData, AttributeDataDTO>();
-=======
-            CreateMap<Comment, CommentDTO>();
-            CreateMap<Category, CategoryDTO>();
->>>>>>> develop
         }
     }
 }
