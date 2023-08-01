680bd7974632fc2635c6d0c7de0f8005e16379eb
Maxence Leroy <113341289+MaxenceLer59@users.noreply.github.com>
2023-08-01 11:30:28 +0200

    Feature/front office history (#36)
    * Feature/front office cartview (#33)
    
    * wip cartcard component
    
    * wip cart page
    
    * add plus minus button logic + delete logic
    
    * wip summary trying to pass dynamic props from cardview
    
    * add dynamic props from cardview to summary component
    
    * add summary component + responsiveness
    
    * add goToCart routing function
    
    * request change
    
    * delete wild console.log
    
    * add HistoryView and CardHistory component and make route for the user order history
    
    ---------
    
    Co-authored-by: Lamalle <78221800+JuLamalle@users.noreply.github.com>
    Co-authored-by: Julien Lamalle <julien1.lamalle@epitech.eu>

--- sell_x_press_front_office/src/components/Layout/HeaderNotRegistered.vue
+++ sell_x_press_front_office/src/components/Layout/HeaderNotRegistered.vue
@@ -20,7 +20,7 @@
             </div>
         </div>
         <div class="header-order">
-            <button class="order-logo-btn">
+            <button class="order-logo-btn" @click="goToCart()">
                 <img class='order-logo' src="../../assets/Header/panier.png" />
             </button>
             <p class="circle-number">0</p>
@@ -35,13 +35,15 @@ export default {
     methods: {
         goToHome() {
             this.$router.push({ path: '/' });
-            window.location.reload();
         },
         goToSignIn() {
             this.$router.push({ path: '/login'});
         },
         goToSignUp() {
             this.$router.push({ path: '/register'});
+        },
+        goToCart() {
+            this.$router.push({ path: '/cart'});
         }
     }
 };
