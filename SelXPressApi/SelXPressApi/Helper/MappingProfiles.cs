f92df9beb1ce7a33febecc5af51f829c541a3b8f
Debray Thomas <thomasdebray83@gmail.com>
2023-08-15 18:56:52 +0200

    adding comment to mapping profile

--- SelXPressApi/SelXPressApi/Helper/MappingProfiles.cs
+++ SelXPressApi/SelXPressApi/Helper/MappingProfiles.cs
@@ -15,8 +15,8 @@ namespace SelXPressApi.Helper
             CreateMap<User, CreateUserDto>();
             CreateMap<Comment, CommentDTO>();
             CreateMap<Category, CategoryDTO>()
-                .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags));
-            CreateMap<Tag, TagDto>();
+                .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags)); // Map Category model to CategoryDTO and include Tags
+            CreateMap<Tag, TagDto>(); // Map Tag model to TagDto
         }
     }
 }
