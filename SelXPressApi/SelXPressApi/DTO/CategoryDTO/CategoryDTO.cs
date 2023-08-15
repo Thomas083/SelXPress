ae170bec04cd98a30bb8f916a89867ffbff018b9
Debray Thomas <thomasdebray83@gmail.com>
2023-08-15 18:55:45 +0200

    Adding summary for category

--- SelXPressApi/SelXPressApi/DTO/CategoryDTO/CategoryDTO.cs
+++ SelXPressApi/SelXPressApi/DTO/CategoryDTO/CategoryDTO.cs
@@ -1,11 +1,26 @@
 ﻿using SelXPressApi.DTO.TagDTO;
+using System.Collections.Generic;
 
 namespace SelXPressApi.DTO.CategoryDTO
 {
+    /// <summary>
+    /// Data transfer object for representing a category.
+    /// </summary>
     public class CategoryDTO
     {
+        /// <summary>
+        /// Gets or sets the ID of the category.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the category.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of tags associated with the category.
+        /// </summary>
         public List<TagDto> Tags { get; set; }
     }
 }
--- SelXPressApi/SelXPressApi/Models/Category.cs
+++ SelXPressApi/SelXPressApi/Models/Category.cs
@@ -1,20 +1,23 @@
-namespace SelXPressApi.Models;
-
-/// <summary>
-/// Model of the category table
-/// </summary>
-public class Category
+namespace SelXPressApi.Models
 {
     /// <summary>
-    /// Id of the category
+    /// Model representing a category.
     /// </summary>
-    public int Id { get; set; }
+    public class Category
+    {
+        /// <summary>
+        /// Gets or sets the ID of the category.
+        /// </summary>
+        public int Id { get; set; }
 
-    /// <summary>
-    /// Name of the category
-    /// </summary>
-    public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the category.
+        /// </summary>
+        public string Name { get; set; }
 
-    public ICollection<Tag> Tags { get; set; }
-
+        /// <summary>
+        /// Gets or sets the collection of tags associated with the category.
+        /// </summary>
+        public ICollection<Tag> Tags { get; set; }
+    }
 }
