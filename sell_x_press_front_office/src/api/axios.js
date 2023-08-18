0ce1ce308ab004c9d6ace58c8c8221c558ee15f1
Maxence Leroy <maxence.leroy@epitech.eu>
2023-08-18 13:39:55 +0200

    add an authorization bearer token if the token was set

--- sell_x_press_front_office/src/api/axios.js
+++ sell_x_press_front_office/src/api/axios.js
@@ -22,22 +22,72 @@ const setRequestConfig = (queryParams) => {
   return config;
 };
 
-export const GET = async (url, queryParams = null) => {
-  return instance.get(url, { ...setRequestConfig(queryParams) });
+export const GET = async (url, token = null, queryParams = null) => {
+  if (token) {
+    return instance.get(url, {
+      ...setRequestConfig(queryParams),
+      headers: {
+        ...instance.defaults.headers,
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } else {
+    return instance.get(url, { ...setRequestConfig(queryParams) });
+  }
 };
 
-export const POST = async (url, data = null, queryParams = null) => {
-  return instance.post(url, data, { ...setRequestConfig(queryParams) });
+export const POST = async (url, data = null, token = null, queryParams = null) => {
+  if (token) {
+    return instance.post(url, data, {
+      ...setRequestConfig(queryParams),
+      headers: {
+        ...instance.defaults.headers,
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } else {
+    return instance.post(url, data, { ...setRequestConfig(queryParams) });
+  }
 };
 
-export const DELETE = async (url, queryParams = null) => {
-  return instance.delete(url, { ...setRequestConfig(queryParams) });
+export const DELETE = async (url, token = null, queryParams = null) => {
+  if (token) {
+    return instance.delete(url, {
+      ...setRequestConfig(queryParams),
+      headers: {
+        ...instance.defaults.headers,
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } else {
+    return instance.delete(url, { ...setRequestConfig(queryParams) });
+  }
 };
 
-export const PUT = async (url, data = null, queryParams = null) => {
-  return instance.put(url, data, { ...setRequestConfig(queryParams) });
+export const PUT = async (url, data = null, token = null, queryParams = null) => {
+  if (token) {
+    return instance.put(url, data, {
+      ...setRequestConfig(queryParams),
+      headers: {
+        ...instance.defaults.headers,
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } else {
+    return instance.put(url, data, { ...setRequestConfig(queryParams) });
+  }
 };
 
-export const PATCH = async (url, data, queryParams = null) => {
-  return instance.patch(url, data, { ...setRequestConfig(queryParams) });
+export const PATCH = async (url, data, token = null, queryParams = null) => {
+  if (token) {
+    return instance.patch(url, data, {
+      ...setRequestConfig(queryParams),
+      headers: {
+        ...instance.defaults.headers,
+        Authorization: `Bearer ${token}`
+      }
+    });
+  } else {
+    return instance.patch(url, data, { ...setRequestConfig(queryParams) });
+  }
 };
