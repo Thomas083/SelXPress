44e25e6d8aa51d49fd5b5f3f560bd03b0cd42e7f
Thomas Debray <thomas.debray@epitech.eu>
2023-08-20 11:18:16 +0200

    Create and Implement the interface and repository

--- SelXPressApi/SelXPressApi/Interfaces/IOrderProductRepository.cs
+++ SelXPressApi/SelXPressApi/Interfaces/IOrderProductRepository.cs
@@ -1,9 +1,17 @@
-﻿using SelXPressApi.Models;
+﻿using SelXPressApi.DTO.OrderDTO;
+using SelXPressApi.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SelXPressApi.Interfaces
 {
-    public interface IOrderProductRepository
-    {
-        ICollection<OrderProduct> GetAllOrderProducts();
-    }
+	public interface IOrderProductRepository
+	{
+		Task<bool> OrderProductExists(int id);
+		Task<OrderProduct> GetOrderProductById(int id);
+		Task<List<OrderProduct>> GetAllOrderProducts();
+		Task<bool> CreateOrderProduct(CreateOrderProductDTO createOrderProduct);
+		Task<bool> UpdateOrderProduct(int id, UpdateOrderProductDTO updateOrderProduct);
+		Task<bool> DeleteOrderProduct(int id);
+	}
 }
--- SelXPressApi/SelXPressApi/Repository/OrderProductRepository.cs
+++ SelXPressApi/SelXPressApi/Repository/OrderProductRepository.cs
@@ -1,4 +1,8 @@
-﻿using SelXPressApi.Data;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SelXPressApi.Data;
+using SelXPressApi.DTO.OrderDTO;
+using SelXPressApi.Helper;
 using SelXPressApi.Interfaces;
 using SelXPressApi.Models;
 
@@ -7,15 +11,65 @@ namespace SelXPressApi.Repository
     public class OrderProductRepository : IOrderProductRepository
     {
         private readonly DataContext _context;
+		private readonly ICommonMethods _commonMethods;
+		private readonly IMapper _mapper;
 
-        public OrderProductRepository(DataContext context)
-        {
-            _context = context;
-        }
+		public OrderProductRepository(DataContext context, ICommonMethods commonMethods, IMapper mapper)
+		{
+			_context = context;
+			_commonMethods = commonMethods;
+			_mapper = mapper;
+		}
+		public Task<bool> OrderProductExists(int id)
+		{
+			return _context.OrderProducts.AnyAsync(o => o.Id == id);
+		}
 
-        public ICollection<OrderProduct> GetAllOrderProducts()
-        {
-            return _context.OrderProducts.OrderBy(op => op.Id).ToList();
-        }
-    }
+		public async Task<bool> CreateOrderProduct(CreateOrderProductDTO createOrderProduct)
+		{
+			var orderProductEntity = _mapper.Map<OrderProduct>(createOrderProduct);
+
+			_context.OrderProducts.Add(orderProductEntity);
+
+			return await _commonMethods.Save();
+		}
+
+		public async Task<bool> DeleteOrderProduct(int id)
+		{
+			if (await OrderProductExists(id))
+			{
+			var orderProduct = await _context.OrderProducts.FirstOrDefaultAsync(o => o.Id == id);
+				_context.OrderProducts.Remove(orderProduct);
+				return await _commonMethods.Save();
+			}
+			return false;
+		}
+
+		public async Task<List<OrderProduct>> GetAllOrderProducts()
+		{
+			return await _context.OrderProducts.ToListAsync();
+		}
+
+		public async Task<OrderProduct?> GetOrderProductById(int id)
+		{
+			return await _context.OrderProducts.FirstOrDefaultAsync(o => o.Id == id);
+		}
+
+
+		public async Task<bool> UpdateOrderProduct(int id, UpdateOrderProductDTO updateOrderProduct)
+		{
+			var orderProduct = await _context.OrderProducts.FindAsync(id);
+
+			if (orderProduct == null)
+				return false;
+
+			// Update properties of the orderProduct entity using values from updateOrderProduct DTO
+			orderProduct.Quantity = updateOrderProduct.Quantity;
+
+			_context.OrderProducts.Update(orderProduct);
+
+			return await _commonMethods.Save();
+		}
+
+	}
 }
