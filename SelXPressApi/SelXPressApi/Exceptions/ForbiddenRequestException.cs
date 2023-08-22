91a0de72bb60dd099b375e2eeff1cd79d8691382
Thomas Debray <thomas.debray@epitech.eu>
2023-08-22 19:08:15 +0200

    Adding Summary and comment to Exception

--- SelXPressApi/SelXPressApi/Exceptions/ForbiddenRequestException.cs
+++ SelXPressApi/SelXPressApi/Exceptions/ForbiddenRequestException.cs
@@ -1,12 +1,17 @@
-﻿namespace SelXPressApi.Exceptions;
-
-/// <summary>
-/// 
-/// </summary>
-public class ForbiddenRequestException : CommonException
+﻿namespace SelXPressApi.Exceptions
 {
-    public ForbiddenRequestException(string message,string code) : base(message, code)
-    {
-        
-    }
+	/// <summary>
+	/// Exception class for forbidden requests.
+	/// </summary>
+	public class ForbiddenRequestException : CommonException
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ForbiddenRequestException"/> class.
+		/// </summary>
+		/// <param name="message">The error message.</param>
+		/// <param name="code">The error code.</param>
+		public ForbiddenRequestException(string message, string code) : base(message, code)
+		{
+		}
+	}
 }
