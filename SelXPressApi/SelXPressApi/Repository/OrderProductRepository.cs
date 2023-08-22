672219697fa759a5b478d5afea1571bd233834dc
Thomas Debray <thomas.debray@epitech.eu>
2023-08-22 19:57:41 +0200

    Adding Summary and comment to repository

--- SelXPressApi/SelXPressApi/Repository/OrderProductRepository.cs
+++ SelXPressApi/SelXPressApi/Repository/OrderProductRepository.cs
@@ -11,12 +11,22 @@ using System.Linq;
 
 namespace SelXPressApi.Repository
 {
+	/// <summary>
+	/// Repository class for managing orders product.
+	/// </summary>
+
 	public class OrderProductRepository : IOrderProductRepository
 	{
 		private readonly DataContext _context;
 		private readonly ICommonMethods _commonMethods;
 		private readonly IMapper _mapper;
 
+		/// <summary>
+		/// Initializes a new instance of the OrderProductRepository class.
+		/// </summary>
+		/// <param name="context">The data context used to interact with the database.</param>
+		/// <param name="commonMethods">Common methods used for data operations.</param>
+		/// <param name="mapper">An AutoMapper instance for object mapping.</param>
 		public OrderProductRepository(DataContext context, ICommonMethods commonMethods, IMapper mapper)
 		{
 			_context = context;
@@ -25,78 +35,114 @@ namespace SelXPressApi.Repository
 		}
 
 		/// <summary>
-		/// Check if an order product with the specified ID exists.
+		/// Checks if an order product with the specified ID exists in the database.
 		/// </summary>
+		/// <param name="id">The ID of the order product.</param>
+		/// <returns>True if an order product with the given ID exists, otherwise false.</returns>
 		public Task<bool> OrderProductExists(int id)
 		{
+			// Returns a task that represents the asynchronous operation to determine
+			// if an order product with the specified ID exists in the database.
 			return _context.OrderProducts.AnyAsync(o => o.Id == id);
 		}
 
 		/// <summary>
-		/// Create a new order product.
+		/// Creates a new order product in the database.
 		/// </summary>
+		/// <param name="createOrderProduct">The data for creating the new order product.</param>
+		/// <returns>True if the order product was created successfully, otherwise false.</returns>
 		public async Task<bool> CreateOrderProduct(CreateOrderProductDTO createOrderProduct)
 		{
+			// Map the data from the DTO to an OrderProduct entity
 			var orderProductEntity = _mapper.Map<OrderProduct>(createOrderProduct);
+
+			// Add the new order product entity to the database context
 			_context.OrderProducts.Add(orderProductEntity);
+
+			// Save changes to the database and return the result asynchronously
 			return await _commonMethods.Save();
 		}
 
 		/// <summary>
-		/// Delete an order product with the specified ID.
+		/// Deletes an order product from the database by its ID.
 		/// </summary>
+		/// <param name="id">The ID of the order product to delete.</param>
+		/// <returns>True if the order product was deleted successfully, otherwise false.</returns>
 		public async Task<bool> DeleteOrderProduct(int id)
 		{
+			// Check if the order product with the specified ID exists
 			if (await OrderProductExists(id))
 			{
+				// Retrieve the order product entity using the provided ID
 				var orderProduct = await _context.OrderProducts.FirstOrDefaultAsync(o => o.Id == id);
+
+				// Remove the order product entity from the database context
 				_context.OrderProducts.Remove(orderProduct);
+
+				// Save changes to the database and return the result asynchronously
 				return await _commonMethods.Save();
 			}
+
+			// If the order product does not exist, return false
 			return false;
 		}
 
 		/// <summary>
-		/// Get all order products.
+		/// Retrieves a list of all order products from the database.
 		/// </summary>
+		/// <returns>A list of all order products.</returns>
 		public async Task<List<OrderProduct>> GetAllOrderProducts()
 		{
+			// Retrieve all order products from the database asynchronously
 			return await _context.OrderProducts.ToListAsync();
 		}
 
 		/// <summary>
-		/// Get all order products associated with a user's email.
+		/// Retrieves a list of order products associated with the specified user's email.
 		/// </summary>
+		/// <param name="email">The email of the user.</param>
+		/// <returns>A list of order products associated with the user's email.</returns>
 		public async Task<List<OrderProduct>> GetOrderProductsByUser(string email)
 		{
+			// Retrieve order products associated with the user's email from the database asynchronously
 			return await _context.OrderProducts
 				.Where(op => op.Order.User.Email == email)
 				.ToListAsync();
 		}
 
 		/// <summary>
-		/// Get an order product by its ID.
+		/// Retrieves an order product by its unique identifier (ID).
 		/// </summary>
+		/// <param name="id">The unique identifier of the order product.</param>
+		/// <returns>The retrieved order product or null if not found.</returns>
 		public async Task<OrderProduct?> GetOrderProductById(int id)
 		{
+			// Retrieve the order product with the specified ID from the database asynchronously
 			return await _context.OrderProducts.FirstOrDefaultAsync(o => o.Id == id);
 		}
 
 		/// <summary>
-		/// Update an existing order product.
+		/// Updates the properties of an existing order product based on the provided information.
 		/// </summary>
+		/// <param name="id">The unique identifier of the order product to be updated.</param>
+		/// <param name="updateOrderProduct">The DTO containing the updated order product information.</param>
+		/// <returns>True if the order product was successfully updated, otherwise false.</returns>
 		public async Task<bool> UpdateOrderProduct(int id, UpdateOrderProductDTO updateOrderProduct)
 		{
+			// Find the order product entity with the specified ID
 			var orderProduct = await _context.OrderProducts.FindAsync(id);
 
+			// Check if the order product exists
 			if (orderProduct == null)
 				return false;
 
-			// Update properties of the orderProduct entity using values from updateOrderProduct DTO
+			// Update the quantity property of the order product entity using values from updateOrderProduct DTO
 			orderProduct.Quantity = updateOrderProduct.Quantity;
 
+			// Mark the order product entity as updated
 			_context.OrderProducts.Update(orderProduct);
 
+			// Save changes to the database using common methods
 			return await _commonMethods.Save();
 		}
 	}
--- SelXPressApi/SelXPressApi/Repository/TagRepository.cs
+++ SelXPressApi/SelXPressApi/Repository/TagRepository.cs
@@ -10,90 +10,107 @@ using System.Threading.Tasks;
 
 namespace SelXPressApi.Repository
 {
-    public class TagRepository : ITagRepository
-    {
-        private readonly DataContext _context;
-        private readonly ICommonMethods _commonMethods;
+	/// <summary>
+	/// Repository for managing tags.
+	/// </summary>
+	public class TagRepository : ITagRepository
+	{
+		private readonly DataContext _context;
+		private readonly ICommonMethods _commonMethods;
 
-        public TagRepository(DataContext context, ICommonMethods commonMethods)
-        {
-            _context = context;
-            _commonMethods = commonMethods;
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TagRepository"/> class.
+		/// </summary>
+		/// <param name="context">The database context.</param>
+		/// <param name="commonMethods">Common methods provider.</param>
+		public TagRepository(DataContext context, ICommonMethods commonMethods)
+		{
+			_context = context;
+			_commonMethods = commonMethods;
+		}
 
-        /// <summary>
-        /// Check if a tag with the given ID exists.
-        /// </summary>
-        public async Task<bool> TagExists(int id)
-        {
-            return await _context.Tags.AnyAsync(t => t.Id == id);
-        }
+		/// <summary>
+		/// Checks if a tag with the specified ID exists.
+		/// </summary>
+		/// <param name="id">The ID of the tag to check.</param>
+		/// <returns>True if the tag exists; otherwise, false.</returns>
+		public async Task<bool> TagExists(int id)
+		{
+			return await _context.Tags.AnyAsync(t => t.Id == id);
+		}
 
-        /// <summary>
-        /// Get all tags with associated categories.
-        /// </summary>
-        public async Task<List<Tag>> GetAllTags()
-        {
-            return await _context.Tags.Include(t => t.Category).ToListAsync();
-        }
+		/// <summary>
+		/// Retrieves a list of all tags including their categories.
+		/// </summary>
+		/// <returns>A list of tags with associated categories.</returns>
+		public async Task<List<Tag>> GetAllTags()
+		{
+			return await _context.Tags.Include(t => t.Category).ToListAsync();
+		}
 
-        /// <summary>
-        /// Get a tag by its ID with the associated category.
-        /// </summary>
-        public async Task<Tag?> GetTagById(int id)
-        {
-            return await _context.Tags.Include(t => t.Category).FirstOrDefaultAsync(t => t.Id == id);
-        }
+		/// <summary>
+		/// Retrieves a tag by its ID including its category.
+		/// </summary>
+		/// <param name="id">The ID of the tag to retrieve.</param>
+		/// <returns>The retrieved tag with associated category, or null if not found.</returns>
+		public async Task<Tag?> GetTagById(int id)
+		{
+			return await _context.Tags.Include(t => t.Category).FirstOrDefaultAsync(t => t.Id == id);
+		}
 
-        /// <summary>
-        /// Create a new tag with the given information.
-        /// </summary>
-        public async Task<bool> CreateTag(CreateTagDTO createTag)
-        {
-            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == createTag.CategoryId);
-            if (category == null)
-            {
-                // Handle the case where the category does not exist
-                return false;
-            }
+		/// <summary>
+		/// Creates a new tag.
+		/// </summary>
+		/// <param name="createTag">DTO containing tag details.</param>
+		/// <returns>True if the tag was created successfully; otherwise, false.</returns>
+		public async Task<bool> CreateTag(CreateTagDTO createTag)
+		{
+			Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == createTag.CategoryId);
+			if (category == null)
+				return false;
 
-            Tag newTag = new Tag
-            {
-                Name = createTag.Name,
-                Category = category
-            };
-            await _context.Tags.AddAsync(newTag);
-            return await _commonMethods.Save();
-        }
+			Tag newTag = new Tag
+			{
+				Name = createTag.Name,
+				Category = category
+			};
+			await _context.Tags.AddAsync(newTag);
+			return await _commonMethods.Save();
+		}
 
-        /// <summary>
-        /// Update an existing tag with new information.
-        /// </summary>
-        public async Task<bool> UpdateTag(int id, UpdateTagDTO updateTag)
-        {
-            if (!await TagExists(id))
-                return false;
+		/// <summary>
+		/// Updates an existing tag's information.
+		/// </summary>
+		/// <param name="id">The ID of the tag to update.</param>
+		/// <param name="updateTag">DTO containing updated tag details.</param>
+		/// <returns>True if the tag was updated successfully; otherwise, false.</returns>
+		public async Task<bool> UpdateTag(int id, UpdateTagDTO updateTag)
+		{
+			if (!await TagExists(id))
+				return false;
 
-            Tag tag = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
+			Tag tag = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
 
-            if (tag != null && updateTag.Name != null && tag.Name != updateTag.Name)
-                tag.Name = updateTag.Name;
+			if (tag != null && updateTag.Name != null && tag.Name != updateTag.Name)
+				tag.Name = updateTag.Name;
 
-            return await _commonMethods.Save();
-        }
+			return await _commonMethods.Save();
+		}
 
-        /// <summary>
-        /// Delete a tag with the given ID.
-        /// </summary>
-        public async Task<bool> DeleteTag(int id)
-        {
-            if (!await TagExists(id))
-                return false;
+		/// <summary>
+		/// Deletes a tag by its ID.
+		/// </summary>
+		/// <param name="id">The ID of the tag to delete.</param>
+		/// <returns>True if the tag was deleted successfully; otherwise, false.</returns>
+		public async Task<bool> DeleteTag(int id)
+		{
+			if (!await TagExists(id))
+				return false;
 
-            Tag tagToDelete = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
-            _context.Tags.Remove(tagToDelete);
+			Tag tagToDelete = await _context.Tags.FirstOrDefaultAsync(t => t.Id == id);
+			_context.Tags.Remove(tagToDelete);
 
-            return await _commonMethods.Save();
-        }
-    }
+			return await _commonMethods.Save();
+		}
+	}
 }
