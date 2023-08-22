af83237bba612ac824c4ca58de70cba81ef68102
Thomas Debray <thomas.debray@epitech.eu>
2023-08-22 19:08:43 +0200

    Adding Summary and comment to UnauthorizedException

--- SelXPressApi/SelXPressApi/Exceptions/UnauthorizedException.cs
+++ SelXPressApi/SelXPressApi/Exceptions/UnauthorizedException.cs
@@ -1,8 +1,17 @@
-﻿namespace SelXPressApi.Exceptions;
-
-public class UnauthorizedException : CommonException
+﻿namespace SelXPressApi.Exceptions
 {
-    public UnauthorizedException(string message, string code) : base(message,code)
-    {
-    }
+	/// <summary>
+	/// Exception class for unauthorized access.
+	/// </summary>
+	public class UnauthorizedException : CommonException
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UnauthorizedException"/> class.
+		/// </summary>
+		/// <param name="message">The error message.</param>
+		/// <param name="code">The error code.</param>
+		public UnauthorizedException(string message, string code) : base(message, code)
+		{
+		}
+	}
 }
