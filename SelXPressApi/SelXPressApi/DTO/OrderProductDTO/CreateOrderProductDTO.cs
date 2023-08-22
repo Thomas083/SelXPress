b64100649c2717795417bdc0e02cca9dc627b7b3
Thomas Debray <thomas.debray@epitech.eu>
2023-08-22 19:00:21 +0200

    Adding summary to DTO

--- SelXPressApi/SelXPressApi/DTO/OrderProductDTO/CreateOrderProductDTO.cs
+++ SelXPressApi/SelXPressApi/DTO/OrderProductDTO/CreateOrderProductDTO.cs
@@ -1,8 +1,18 @@
 namespace SelXPressApi.DTO.OrderDTO
 {
+	/// <summary>
+	/// DTO for creating an order product.
+	/// </summary>
 	public class CreateOrderProductDTO
 	{
+		/// <summary>
+		/// Gets or sets the ID of the product for the order.
+		/// </summary>
 		public int ProductId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the quantity of the product for the order.
+		/// </summary>
 		public int Quantity { get; set; }
 	}
 }
