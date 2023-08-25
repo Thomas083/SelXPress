6546664ef9ffaecc918a1e9902971e3fef325c25
Maxence Leroy <maxence.leroy@epitech.eu>
2023-08-25 13:39:35 +0200

    add method and integration (beginning) for the carousel home

--- sell_x_press_front_office/src/components/Home/HomeCarrousel.vue
+++ sell_x_press_front_office/src/components/Home/HomeCarrousel.vue
@@ -1,44 +1,81 @@
 <template>
     <div id="carouselExample" class="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                </div>
-                <div class="carousel-item">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                    <img src="../../assets/categories-product-maquette-1.jpg" alt="...">
-                </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img v-for="product in carrouselProduct.slice(0,Math.ceil(carrouselProduct.length/3))" :src="product.picture" v-on:click="goToProduct(product.id, product.name)" alt="...">
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+            <div class="carousel-item">
+                <img v-for="product in carrouselProduct.slice(Math.ceil(carrouselProduct.length/3),Math.ceil(carrouselProduct.length/6)+Math.ceil(carrouselProduct.length/3)+1)" v-on:click="goToProduct(product.id, product.name)" :src="product.picture" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img v-for="product in carrouselProduct.slice(Math.ceil(carrouselProduct.length/6)+Math.ceil(carrouselProduct.length/3)+1,carrouselProduct.length)" v-on:click="goToProduct(product.id, product.name)" :src="product.picture" alt="...">
+            </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+
+import { GET } from '@/api/axios';
+import { ENDPOINTS } from '@/api/endpoints';
+
+export default {
+    name: 'HomeCarrousel',
+    data() {
+        return {
+            products: [
+                {id: 1, name: 'test1', picture: 'https://m.media-amazon.com/images/I/612PtqPxOiL.__AC_SX300_SY300_QL70_ML2_.jpg'},
+                {id: 2, name: 'test2', picture: 'https://m.media-amazon.com/images/I/61UkKXVBdIL._AC_SX679_.jpg'},
+                {id: 3, name: 'test3', picture: 'https://m.media-amazon.com/images/I/614NHZvRgeL._AC_SX679_.jpg'},
+                {id: 4, name: 'test4', picture: 'https://m.media-amazon.com/images/I/612PtqPxOiL.__AC_SX300_SY300_QL70_ML2_.jpg'},
+                {id: 5, name: 'test5', picture: 'https://m.media-amazon.com/images/I/612PtqPxOiL.__AC_SX300_SY300_QL70_ML2_.jpg'},
+            ],
+            numProduct: 12,
+        }
+    },
+    methods: {
+        shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        },
+        goToProduct(id, name) {
+            this.$router.push({ path: `/product/${id}/${name}`})
+        }
+    },
+    computed: {
+        carrouselProduct() {
+            if (this.products && this.products.length <= this.numProduct) {
+                return this.products;
+            } else if (this.products && this.products.length > this.numProduct) {
+                const shuffledProducts = [...this.products];
+                this.shuffleArray(shuffledProducts);
+                return shuffledProducts.slice(0, this.numProduct);
+            }
+        }
+    },
+    // mounted() {
+    //     GET(ENDPOINTS.GET_ALL_PRODUCTS)
+    //         .then((response) => {
+    //             this.products = response.data
+    //         })
+    //         .catch((error) => {
+    //             console.dir(error)
+    //         });
+    // },
+}
 </script>
 
 <style scoped>
-
 .carousel-item.active {
     display: flex;
     flex-direction: row;
@@ -61,5 +98,4 @@ img {
     height: 20vh;
     cursor: pointer;
 }
-
 </style>
