026687d7724984fb64d0efbdc141f5c2a96510bf
ugo-bertrand <113342194+ugo-bertrand@users.noreply.github.com>
2023-08-28 15:42:56 +0200

    cart controller unit test

---
+++ SelXPressApi/SelXPressApiTest/CartControllerTest/CreateCartByUserTest.cs
@@ -0,0 +1,80 @@
+﻿using FakeItEasy;
+using SelXPressApi.Controllers;
+using SelXPressApi.Interfaces;
+using SelXPressApi.Middleware;
+
+namespace SelXPressApiTest.CartControllerTest;
+
+public class CreateCartByUserTest
+{
+    private CartController _cartController;
+    private ICartRepository _cartRepository;
+    private IAuthorizationMiddleware _authorizationMiddleware;
+
+    /// <summary>
+    /// Initialize a new instance of the <see cref="CreateCartByAdminTest"/>
+    /// </summary>
+    public CreateCartByUserTest()
+    {
+        _cartRepository = A.Fake<ICartRepository>();
+        _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+
+        _cartController = new CartController(_cartRepository, _authorizationMiddleware);
+    }
+
+    /// <summary>
+    /// Test to check the status of the request is equals to 200
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_200()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check the status of the request is equals to 400 (BadRequest)
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_400()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check the status of the request is equals to 401 because the token is missing
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_401_TokenIsMissing()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check the status of the request is equals to 401 because the token is invalid
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_401_TokenIsInvalid()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check the status of the request is equals to 401 because the email is not in the database
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_401_EmailIsNotInTheDatabase()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 403
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_403()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 500 due to an internal server error
+    /// </summary>
+    public void CartController_CreateCartByUser_Status_500()
+    {
+        //todo
+    }
+}
---
+++ SelXPressApi/SelXPressApiTest/CartControllerTest/DeleteCartTest.cs
@@ -0,0 +1,73 @@
+﻿using FakeItEasy;
+using SelXPressApi.Controllers;
+using SelXPressApi.Interfaces;
+using SelXPressApi.Middleware;
+
+namespace SelXPressApiTest.CartControllerTest;
+
+public class DeleteCartTest
+{
+    private CartController _cartController;
+    private ICartRepository _cartRepository;
+    private IAuthorizationMiddleware _authorizationMiddleware;
+
+    /// <summary>
+    /// Initialize a new instance of the <see cref="DeleteCartTest"/>
+    /// </summary>
+    public DeleteCartTest()
+    {
+        _cartRepository = A.Fake<ICartRepository>();
+        _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+
+        _cartController = new CartController(_cartRepository, _authorizationMiddleware);
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 200
+    /// </summary>
+    public void CartController_DeleteCart_Status_200()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 400 (BadRequest)
+    /// </summary>
+    public void CartController_DeleteCart_Status_400()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is missing
+    /// </summary>
+    public void CartController_DeleteCart_Status_401_TokenIsMissing()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is invalid
+    /// </summary>
+    public void CartController_DeleteCart_Status_401_TokenIsInvalid()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the email is not in the database
+    /// </summary>
+    public void CartController_DeleteCart_EmailIsNotInTheDatabase()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 403
+    /// </summary>
+    public void CartController_DeleteCart_Status_403()
+    {
+        //todo
+    }
+    
+}
