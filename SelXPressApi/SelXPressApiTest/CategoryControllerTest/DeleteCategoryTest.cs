6f3a0f924b861b42f56138a9e340bb4196862d97
ugo-bertrand <113342194+ugo-bertrand@users.noreply.github.com>
2023-08-29 10:06:34 +0200

    add the unit test template to the category controller

---
+++ SelXPressApi/SelXPressApiTest/CategoryControllerTest/DeleteCategoryTest.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using SelXPressApi.Controllers;
+using SelXPressApi.Interfaces;
+using SelXPressApi.Middleware;
+
+namespace SelXPressApiTest.CategoryControllerTest;
+
+public class DeleteCategoryTest
+{
+    private CategoryController _categoryController;
+    private ICategoryRepository _categoryRepository;
+    private IMapper _mapper;
+    private IAuthorizationMiddleware _authorizationMiddleware;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DeleteCategoryTest"/>
+    /// </summary>
+    public DeleteCategoryTest()
+    {
+        _categoryRepository = A.Fake<ICategoryRepository>();
+        _mapper = A.Fake<IMapper>();
+        _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+
+        _categoryController = new CategoryController(_categoryRepository, _mapper, _authorizationMiddleware);
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 200 
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_200()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 400 (BadRequest)
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_400()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is missing
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_401_TokenIsMissing()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is invalid
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_401_TokenIsInvalid()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the email is not in the database
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_401_EmailIsNotInTheDatabase()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 403
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_403()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 404
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_404()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 500 due to an internal server error
+    /// </summary>
+    public void CategoryController_DeleteCategory_Status_500()
+    {
+        //todo
+    }
+}
---
+++ SelXPressApi/SelXPressApiTest/CategoryControllerTest/GetCategoriesTest.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using SelXPressApi.Controllers;
+using SelXPressApi.Interfaces;
+using SelXPressApi.Middleware;
+
+namespace SelXPressApiTest.CategoryControllerTest;
+
+/// <summary>
+/// Class to test the api/Category/ GET route
+/// </summary>
+public class GetCategoriesTest
+{
+    private CategoryController _categoryController;
+    private ICategoryRepository _categoryRepository;
+    private IMapper _mapper;
+    private IAuthorizationMiddleware _authorizationMiddleware;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetCategoriesTest"/>
+    /// </summary>
+    public GetCategoriesTest()
+    {
+        _categoryRepository = A.Fake<ICategoryRepository>();
+        _mapper = A.Fake<IMapper>();
+        _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+
+        _categoryController = new CategoryController(_categoryRepository, _mapper, _authorizationMiddleware);
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 200
+    /// </summary>
+    public void CategoryController_GetCategories_Status_200()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 400 (BadRequest)
+    /// </summary>
+    public void CategoryController_GetCategories_Status_400()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 404
+    /// </summary>
+    public void CategoryController_GetCategories_Status_404()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 500 die to an internal server error
+    /// </summary>
+    public void CategoryController_GetCategories_Status_500()
+    {
+        //todo
+    }
+}
