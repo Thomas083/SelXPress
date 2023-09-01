002d235ddd75868c52290d4ce2287a6ce922437e
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-09-01 21:44:24 +0200

    add the template of the unit tests for the RoleController

---
+++ SelXPressApi/SelXPressApiTest/RoleControllerTest/CreateRoleTest.cs
@@ -0,0 +1,77 @@
+﻿using FakeItEasy;
+using SelXPressApi.Controllers;
+using SelXPressApi.Interfaces;
+using SelXPressApi.Middleware;
+
+namespace SelXPressApiTest.RoleControllerTest;
+
+public class CreateRoleTest
+{
+    private RoleController _roleController;
+    private IRoleRepository _roleRepository;
+    private IAuthorizationMiddleware _authorizationMiddleware;
+
+    public CreateRoleTest()
+    {
+        _roleRepository = A.Fake<IRoleRepository>();
+        _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+
+        _roleController = new RoleController(_roleRepository, _authorizationMiddleware);
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 201
+    /// </summary>
+    public void RoleController_CreateRole_Status_201()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 400
+    /// </summary>
+    public void RoleController_CreateRole_Status_400()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is missing
+    /// </summary>
+    public void RoleController_CreateRole_Status_401_TokenIsMissing()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is invalid
+    /// </summary>
+    public void RoleController_CreateRole_Status_401_TokenIsInvalid()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the email is not in the database
+    /// </summary>
+    public void RoleController_CreateRole_Status_401_EmailIsNotInTheDatabase()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 403
+    /// </summary>
+    public void RoleController_CreateRole_Status_403()
+    {
+        //todo
+    }
+    
+    /// <summary>
+    /// Test to check if the status of the request is equals to 500 due to an internal server error
+    /// </summary>
+    public void RoleController_CreateRole_Status_500()
+    {
+        //todo
+    }
+}
---
+++ SelXPressApi/SelXPressApiTest/RoleControllerTest/UpdateRoleTest.cs
@@ -0,0 +1,85 @@
+﻿using FakeItEasy;
+using SelXPressApi.Controllers;
+using SelXPressApi.Interfaces;
+using SelXPressApi.Middleware;
+
+namespace SelXPressApiTest.RoleControllerTest;
+
+public class UpdateRoleTest
+{
+    private RoleController _roleController;
+    private IRoleRepository _roleRepository;
+    private IAuthorizationMiddleware _authorizationMiddleware;
+
+    public UpdateRoleTest()
+    {
+        _roleRepository = A.Fake<IRoleRepository>();
+        _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+
+        _roleController = new RoleController(_roleRepository, _authorizationMiddleware);
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 200
+    /// </summary>
+    public void RoleController_UpdateRole_Status_200()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 400
+    /// </summary>
+    public void RoleController_UpdateRole_Status_400()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is missing
+    /// </summary>
+    public void RoleController_UpdateRole_Status_401_TokenIsMissing()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the token is invalid
+    /// </summary>
+    public void RoleController_UpdateRole_Status_401_TokenIsInvalid()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 401 because the email is not in the database
+    /// </summary>
+    public void RoleController_UpdateRole_Status_401_EmailIsNotInTheDatabase()
+    {
+        //todo
+    }
+
+    /// <summary>
+    /// Test to check if the status of the request is equals to 403
+    /// </summary>
+    public void RoleController_UpdateRole_Status_403()
+    {
+        //todo
+    }
+    
+    /// <summary>
+    /// Test to check if the status of the request is equals to 404
+    /// </summary>
+    public void RoleController_UpdateRole_Status_404()
+    {
+        //todo
+    }
+    
+    /// <summary>
+    /// Test to check if the status of the request is equals to 500 due to an internal server error
+    /// </summary>
+    public void RoleController_UpdateRole_Status_500()
+    {
+        //todo
+    }
+}
