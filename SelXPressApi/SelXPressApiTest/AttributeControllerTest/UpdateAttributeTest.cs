22e222b486a04f3aa4c1ead2874891b94dbba95e
ugo-bertrand <ugo.bertrand@epitech.eu>
2023-09-09 17:47:34 +0200

    write the unit test for the attribute controller

--- SelXPressApi/SelXPressApiTest/AttributeControllerTest/UpdateAttributeTest.cs
+++ SelXPressApi/SelXPressApiTest/AttributeControllerTest/UpdateAttributeTest.cs
@@ -1,6 +1,10 @@
 ﻿using AutoMapper;
 using FakeItEasy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using SelXPressApi.Controllers;
+using SelXPressApi.DTO.AttributeDTO;
+using SelXPressApi.Exceptions;
 using SelXPressApi.Interfaces;
 using SelXPressApi.Middleware;
 
@@ -15,6 +19,7 @@ public class UpdateAttributeTest
     private IAttributeRepository _attributeRepository;
     private IMapper _mapper;
     private IAuthorizationMiddleware _authorizationMiddleware;
+    private HttpContext _httpContext;
 
     /// <summary>
     /// Initialize a new instance of the <see cref="UpdateAttributeTest"/>
@@ -24,78 +29,147 @@ public class UpdateAttributeTest
         _attributeRepository = A.Fake<IAttributeRepository>();
         _mapper = A.Fake<IMapper>();
         _authorizationMiddleware = A.Fake<IAuthorizationMiddleware>();
+        _httpContext = A.Fake<HttpContext>();
+        
         _attributeController = new AttributeController(_attributeRepository, _mapper, _authorizationMiddleware);
+        var controllerContext = new ControllerContext()
+        {
+            HttpContext = _httpContext
+        };
+        _attributeController.ControllerContext = controllerContext;
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 200
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_200()
+    public async void AttributeController_UpdateAttribute_Status_200()
     {
-        //todo
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() { Name = "test", Type = "test" };
+        A.CallTo(() => _authorizationMiddleware.CheckIfTokenExists(_httpContext)).Returns(true);
+        A.CallTo(() => _authorizationMiddleware.CheckRoleIfAdmin(_httpContext)).Returns(true);
+        A.CallTo(() => _attributeRepository.AttributeExists(1)).Returns(true);
+
+        var result = await _attributeController.UpdateAttribute(1, requestBody);
+
+        Assert.IsType<OkResult>(result);
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 400
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_400()
+    public async void AttributeController_UpdateAttribute_Status_400()
     {
-        //todo
+        A.CallTo(() => _authorizationMiddleware.CheckIfTokenExists(_httpContext)).Returns(true);
+        A.CallTo(() => _authorizationMiddleware.CheckRoleIfAdmin(_httpContext)).Returns(true);
+        A.CallTo(() => _attributeRepository.AttributeExists(1)).Returns(true);
+        var exception = await Assert.ThrowsAsync<BadRequestException>(() =>_attributeController.UpdateAttribute(1,null));
+        
+        Assert.Equal("Some fields are missing, please try again with complete data", exception.Message);
+        Assert.Equal("ATT-1102",exception.Code);
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 401 because the token is missing
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_401_TokenIsMissing()
+    public async void AttributeController_UpdateAttribute_Status_401_TokenIsMissing()
     {
-        //todo
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() {Name = "test",Type = "test"};
+
+        A.CallTo(() => _authorizationMiddleware.CheckIfTokenExists(_httpContext)).Throws(
+            new UnauthorizedException("The token is not valid, please try again with another token", "SRV-1702"));
+
+        _attributeController.ControllerContext = new ControllerContext { HttpContext = _httpContext };
+
+        var exception =
+            await Assert.ThrowsAsync<UnauthorizedException>(() => _attributeController.UpdateAttribute(1,requestBody));
+        Assert.Equal("The token is not valid, please try again with another token", exception.Message);
+        Assert.Equal("SRV-1702", exception.Code);
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 401 because the token is invalid
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_401_TokenIsInvalid()
+    public async void AttributeController_UpdateAttribute_Status_401_TokenIsInvalid()
     {
-        //todo
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() {Name = "test",Type = "test"};
+
+        A.CallTo(() => _authorizationMiddleware.CheckIfTokenExists(_httpContext)).Throws(
+            new UnauthorizedException("An error occured while the decoding of the jwt token", "SRV-1701"));
+
+        _attributeController.ControllerContext = new ControllerContext { HttpContext = _httpContext };
+
+        var exception =
+            await Assert.ThrowsAsync<UnauthorizedException>(() => _attributeController.UpdateAttribute(1,requestBody));
+        Assert.Equal("An error occured while the decoding of the jwt token", exception.Message);
+        Assert.Equal("SRV-1701", exception.Code);
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 401 because the email is not in the database
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_401_EmailIsNotInTheDatabase()
+    public async void AttributeController_UpdateAttribute_Status_401_EmailIsNotInTheDatabase()
     {
-        //todo
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() {Name = "test",Type = "test"};
+
+        A.CallTo(() => _authorizationMiddleware.CheckIfTokenExists(_httpContext)).Throws(
+            new UnauthorizedException("The email address is not in the database","SRV-1401"));
+
+        _attributeController.ControllerContext = new ControllerContext { HttpContext = _httpContext };
+
+        var exception =
+            await Assert.ThrowsAsync<UnauthorizedException>(() => _attributeController.UpdateAttribute(1,requestBody));
+        Assert.Equal("The email address is not in the database", exception.Message);
+        Assert.Equal("SRV-1401", exception.Code);
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 403
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_403()
+    public async void AttributeController_UpdateAttribute_Status_403()
     {
-        //todo
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() {Name = "test",Type = "test"};
+        
+        A.CallTo(() => _authorizationMiddleware.CheckRoleIfAdmin(_httpContext)).Returns(false);
+
+        var exception = await Assert.ThrowsAsync<ForbiddenRequestException>(() => _attributeController.UpdateAttribute(1,requestBody));
+        
+        Assert.Equal("You are not authorized to perform this operation", exception.Message);
+        Assert.Equal("ATT-2001", exception.Code );
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 404
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_404()
+    public async void AttributeController_UpdateAttribute_Status_404()
     {
-        //todo
+        int id = 1;
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() { Name = "test", Type = "test" };
+        A.CallTo(() => _authorizationMiddleware.CheckIfTokenExists(_httpContext)).Returns(true);
+        A.CallTo(() => _authorizationMiddleware.CheckRoleIfAdmin(_httpContext)).Returns(true);
+        A.CallTo(() => _attributeRepository.AttributeExists(id)).Returns(false);
+
+        var exception = await Assert.ThrowsAsync<NotFoundException>(() => _attributeController.UpdateAttribute(id,requestBody));
+        Assert.Equal("The attribute with ID " + id + " doesn't exist", exception.Message);
+        Assert.Equal("ATT-1402", exception.Code);
     }
 
     /// <summary>
     /// Test to check if the status of the request is equals to 500
     /// </summary>
     [Fact]
-    public void AttributeController_UpdateAttribute_Status_500()
+    public async void AttributeController_UpdateAttribute_Status_500()
     {
-        //todo
+        UpdateAttributeDTO requestBody = new UpdateAttributeDTO() {Name = "test",Type = "test"};
+        A.CallTo(() => _attributeRepository.UpdateAttribute(1,requestBody)).Throws(new Exception("SRV-1000"));
+        var exception = await Assert.ThrowsAsync<Exception>(() => _attributeRepository.UpdateAttribute(1,requestBody));
+        
+        Assert.Equal("SRV-1000",exception.Message);
     }
 }
